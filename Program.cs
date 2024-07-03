2024d6ad5fe4beb8324a1ede5246234cc09aef61
harineGirishankaran <160748850+harineGirishankaran@users.noreply.github.com>
2024-07-03 14:12:57 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,93 +1,57 @@
 using System; 
-
 using System.Collections.Generic; 
-
 using System.ComponentModel; 
-
-using System.Linq; 
-
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text; 
-
 using System.Threading.Tasks; 
-
-  
-
 namespace Y9_DEC_TO_BIN_SKELETON 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
 
-{ 
-
-    internal class Program 
-
-    { 
-
-        static void Main(string[] args) 
-
-        { 
+        {
 
             //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode) 
-
             //int myInt = 0; //just for testing single stepping 
-
             //string myString = "12"; //watch me being cast from string to int 
-
             // int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int 
-
             //Console.WriteLine(myStringAsInt); 
-
             //string twelve = "is twelve"; 
-
             //Console.WriteLine(myString = myString + " " + twelve); 
 
             //MAIN:  NUMBER CONVERSION PROGRAM 
-
-  
-
             //CODE GOES HERE 
 
-            Console.WriteLine("Enter a number"); 
+            Console.WriteLine("welcome");
 
-                int inputNumber = Convert.ToInt32(Console.ReadLine()); 
 
-            string[] binaryNumber = { "0", "0", "0", "0", "0", "0", "0", "0", }; 
+        }
+            //static void means the function will not return a value so it does not need a data type  
+            //...this function DOES return a value so the method must have a data type 
 
-            for (int i = 0; i < binaryNumber.Length; i++) 
+             static string numberConversion(int number, int numberbase)
+            {
+                Console.WriteLine("Enter a number");
+                int inputNumber = Convert.ToInt32(Console.ReadLine());
+                int[] binaryNumber = { 0, 0, 0, 0, 0, 0, 0, 0, };
+                for (int i = 0; i < binaryNumber.Length; i++)
+                {
+                    binaryNumber[i] = 2 % inputNumber;
+                    inputNumber = inputNumber / 2;
+                }
 
-            { 
+                for (int i = 0; i < binaryNumber.Length; i++)
 
-                string result = numberConversion 
+                {
+                    Convert.ToString(binaryNumber[i]);
+                }
+                string result = binaryNumber[7] + binaryNumber[6] + binaryNumber[5] + binaryNumber[4] + binaryNumber[3] + binaryNumber[2] + binaryNumber[1] + binaryNumber[0];
+                return result; //REMOVE THE RED LINE! 
 
-            } 
+                Console.ReadLine();
 
-            
-
-  
-
-  
-
-  
-
-        } 
-
-  
-
-        //static void means the function will not return a value so it does not need a data type  
-
-        //...this function DOES return a value so the method must have a data type 
-
-          static string numberConversion(int number, int numberbase) 
-
-         { 
-
-        return result; //REMOVE THE RED LINE! 
-
-         Console.ReadLine(); 
-
-    } 
-
-  
-
-    } 
-
-  
-
-} 
+            }
+        }
+    }
